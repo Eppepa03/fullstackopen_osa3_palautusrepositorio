2bd55e5def81d7e70ae2245c898eaedf84877d15
Eppepa03 <162826542+Eppepa03@users.noreply.github.com>
2024-12-08 16:42:45 +0200

    uibuild

--- puhelinluettelo_osa3_backend/models/person.js
+++ puhelinluettelo_osa3_backend/models/person.js
@@ -5,38 +5,38 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 mongoose.connect(url)
-    .then(result => {
-        console.log('connected to MongoDB')
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB: ', error.message)
-    })
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB: ', error.message)
+  })
 
 const noteSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minlength: 3,
-        required: true
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (value) => {
+        return /^\d{2,3}-\d{7,}$/.test(value)
+      },
+      message: props => `${props.value} is not a valid phone number!`
     },
-    number: {
-        type: String,
-        validate: {
-            validator: (value) => {
-                return /^\d{2,3}-\d{7,}$/.test(value)
-            },
-            message: props => `${props.value} is not a valid phone number!`
-        },
-        minlength: 8,
-        required: true
-    }
+    minlength: 8,
+    required: true
+  }
 })
 
 noteSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-        returnedObject.id = returnedObject._id.toString()
-        delete returnedObject._id
-        delete returnedObject.__v
-    }
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
 })
 
 module.exports = mongoose.model('Person', noteSchema)
